8280639dc6e27bed46c4f5b6703da957e360b1f6
slawekSk13 <slawomir.skrzeczynski@gmail.com>
2021-09-01 14:11:21 +0200

    validation added

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Formik} from 'formik';
+import * as Yup from 'yup';
 
 const Form = ({initialValues, handleSubmit, handleCancel}) => {
     return (
@@ -7,11 +8,14 @@ const Form = ({initialValues, handleSubmit, handleCancel}) => {
             title: '',
             body: '',
             userId: Math.floor(Math.random()*10) + 1
-        }} onSubmit={handleSubmit}>
+        }} validationSchema={Yup.object({
+            title: Yup.string().min(3, 'Title must be longer than 3 letters').required('Please add title'),
+            body: Yup.string().min(15, 'Post must be longer than 15 letters').required('Please add post')
+        })} onSubmit={handleSubmit}>
             {props => (
                 <form className='box' onSubmit={props.handleSubmit}>
                     <div className='field'>
-                        <label className="label">Name</label>
+                        <label className="label">Title</label>
                         <div className="control">
                         <input
                             className='input'
@@ -21,7 +25,9 @@ const Form = ({initialValues, handleSubmit, handleCancel}) => {
                         value={props.values.title}
                         name='title'
                             placeholder='Post title'
-                    /></div></div>
+                    />
+                            {props.touched.title && props.errors.title ? <div className='notification is-danger'>{props.errors.title}</div> : null}
+                        </div></div>
                     <div className='field'>
                         <label className="label">Post</label>
                         <div className="control">
@@ -32,10 +38,11 @@ const Form = ({initialValues, handleSubmit, handleCancel}) => {
                         value={props.values.body}
                         name='body'
                         placeholder='Enter your text'
-                    /></div></div>
+                    />
+                            {props.touched.body && props.errors.body ? <div className='notification is-danger'>{props.errors.body}</div> : null}</div></div>
                     <div className='buttons'>
                     <button type='submit' className='button is-success'>Send</button>
-                    <button onClick={handleCancel} className='button is-danger'>Cancel</button>
+                    <button onClick={handleCancel} className='button is-warning'>Cancel</button>
                     </div>
                 </form>
             )}
